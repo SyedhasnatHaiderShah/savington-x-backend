0eb42f04ad7cba9b8158d61e1cdae4d3338741ef
Athif <mohammad@wecover.online>
2024-03-01 21:35:18 +0400

    changed port

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -25,7 +25,7 @@ export const getDatabaseDataSourceOptions = ({
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: '65.0.21.91',
-  port: 5432,
+  port: 5444,
   username: 'prod_admin',
   password: 'SavingTon@DB',
   database: 'savington-dev',
